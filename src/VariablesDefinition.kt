e8e8d6ead6709cbdde0ad0209c165f4954f4d586
George Ng'ethe <georgegitz@gmail.com>
2017-11-06 23:05:15 +0200

    Variable code

---
+++ src/VariablesDefinition.kt
@@ -0,0 +1,11 @@
+fun main (args: Array<String>){
+/*
+* variables = a little bucket where we can put information
+* */
+
+    var name = "George"
+    var lastname = "Gitz"
+
+    println(name + " "+ lastname)
+    println(lastname)
+}
