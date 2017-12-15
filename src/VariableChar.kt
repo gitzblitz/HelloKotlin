1b9f127370dbf62a2a3925a81377a993fa05f80e
George Ng'ethe <georgegitz@gmail.com>
2017-12-15 16:38:48 +0200

    Variable char

---
+++ src/VariableChar.kt
@@ -0,0 +1,12 @@
+fun main (args: Array<String>){
+/*
+* variables = a little bucket where we can put information
+* Types: string ,int ,boolean, double, float, char
+* Float = fraction 0.6, 1.2
+* */
+
+    var myChar = 'M'
+    var newChar: Char? = null
+    newChar = 'b'
+    println("$newChar $myChar")
+}
