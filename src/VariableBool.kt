7f5732fabf0139cc1f7e52a6d30a2eed215c8a2d
George Ng'ethe <georgegitz@gmail.com>
2017-12-15 16:08:35 +0200

    Variable Boolean

---
+++ src/VariableBool.kt
@@ -0,0 +1,13 @@
+fun main (args: Array<String>){
+/*
+* variables = a little bucket where we can put information
+* Types: string ,int ,boolean, double, float, char
+*   Boolean = True or False
+ *  */
+
+    var myBool = true
+    var newBool: Boolean? = null
+    newBool = false
+    println(newBool)
+
+}
