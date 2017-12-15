3d78a70af939a344e331ca8baba3a984459eb233
George Ng'ethe <georgegitz@gmail.com>
2017-12-15 15:59:24 +0200

    Variable Int

---
+++ src/VariableTypeInt.kt
@@ -0,0 +1,13 @@
+fun main (args: Array<String>){
+/*
+* variables = a little bucket where we can put information
+* Types: string ,int ,boolean, double, float, char
+* */
+
+    var name:String? = null
+    name = "Gitz"
+   var age = 29
+    var myAge:Int = 30
+
+    println("$name is $age")
+}
