697fcb71482c36c99be8596eeb08160587d80b9d
George Ng'ethe <georgegitz@gmail.com>
2017-12-15 17:45:00 +0200

    Read input

---
+++ src/ReadInput.kt
@@ -0,0 +1,29 @@
+fun main(args: Array<String>){
+
+    println("========================")
+    println("->Welcome to our store<-")
+    println("========================")
+
+    println("Hello! What can we do for you?")
+    var customerResponse = readLine()
+
+   println("Yes! we gonna hook you up with some bread.")
+    var customerResponse2 = readLine()
+
+    println("Now piss off")
+
+    println("=================")
+    println("=================")
+    println("Your conversation with the clerk")
+
+    println("Clerk: Hello! What can we do for you?")
+    println("Customer: $customerResponse")
+
+    println("Clerk: Yes! we gonna hook you up with some bread.")
+    println("Customer: $customerResponse2")
+
+    println("Clerk: Good bye")
+
+
+
+}
