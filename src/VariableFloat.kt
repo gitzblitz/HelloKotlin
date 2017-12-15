25c57d622bdf24dd9d1f1bcff3582c115dffe7a3
George Ng'ethe <georgegitz@gmail.com>
2017-12-15 16:24:22 +0200

    Variable float

---
+++ src/VariableFloat.kt
@@ -0,0 +1,14 @@
+fun main (args: Array<String>){
+/*
+* variables = a little bucket where we can put information
+* Types: string ,int ,boolean, double, float, char
+* Float = fraction 0.6, 1.2
+* */
+
+  var myFloat = 0.5
+    var newFloat: Float? = null
+
+    newFloat = 0.4f
+
+    println(newFloat)
+}
