5f328762ee078825ad35b70cb4e2c51dd6e43b44
George Ng'ethe <georgegitz@gmail.com>
2017-12-15 15:59:11 +0200

    Variable string

---
+++ src/VariableTypeString.kt
@@ -0,0 +1,12 @@
+fun main (args: Array<String>){
+/*
+* variables = a little bucket where we can put information
+* Types: string ,int ,boolean, double, float, char
+* */
+
+    var country:String = "Spain"
+
+
+    println(country)
+
+}
