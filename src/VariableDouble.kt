c45fdc3590fdf548f39831cd23f0109504813bc1
George Ng'ethe <georgegitz@gmail.com>
2017-12-15 16:01:16 +0200

    Variable double

---
+++ src/VariableDouble.kt
@@ -0,0 +1,14 @@
+fun main (args: Array<String>){
+/*
+* variables = a little bucket where we can put information
+* Types: string ,int ,boolean, double, float, char
+* */
+
+    var myDouble =23.00
+    var newDouble: Double? = null
+
+    newDouble = 23.0
+
+    println("here is my $newDouble")
+
+}
