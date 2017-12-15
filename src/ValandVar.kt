be642085bae1cd148c86b53d23366b06b147ce83
George Ng'ethe <georgegitz@gmail.com>
2017-12-15 17:23:18 +0200

    val and var

---
+++ src/ValandVar.kt
@@ -0,0 +1,16 @@
+fun main (args: Array<String>){
+/*
+* Difference between val and var
+* val is a static - not meant to be changed
+* var can change and reassigned
+* */
+
+    val pi = 3.14
+     val url = "www.google.com"
+   var myName = "George"
+    myName = "Gitz"
+    myName = "Something"
+
+
+    println("$pi and $url")
+}
