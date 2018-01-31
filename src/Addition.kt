dfcf268f9d880b0514d5505b85899958988f15ad
George Ng'ethe <georgegitz@gmail.com>
2018-01-31 11:48:00 +0200

    Addition

---
+++ src/Addition.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>){
+
+    /* Addition*/
+    var firstNum = 10
+    var secondNum = 20
+
+    var result:Int?
+
+    result = firstNum + secondNum
+
+    println("This sum is ${firstNum + secondNum}")
+
+
+}
