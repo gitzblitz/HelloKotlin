b70915e18a0af813f25fd82170efc421f1fbf596
George Ng'ethe <georgegitz@gmail.com>
2018-01-31 12:28:29 +0200

    Multiplication

---
+++ src/Multiplication.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>){
+
+    /*
+    Multiplication(*)
+    */
+    var firstNum = 5
+    var secondNum = 15
+
+//    var result:Int?
+//
+//    result = firstNum + secondNum
+
+    println("This result is ${firstNum * secondNum}")
+
+
+}
