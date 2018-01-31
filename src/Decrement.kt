10e5bed1c9e3caf4fbd5a4afe6e36166b8af59cf
George Ng'ethe <gitzblitz@users.noreply.github.com>
2018-01-31 14:46:56 +0200

    Increment and decrement

---
+++ src/Decrement.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+
+    /*
+    increment(-)
+    */
+    var firstNum = 8
+    var secondNum = 3
+
+//    var result:Int?
+//
+
+    var inc = firstNum + 1
+    var decrement = firstNum -1
+
+    println(--firstNum)
+
+
+
+}
---
+++ src/Increment.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+
+    /*
+    increment(+)
+    */
+    var firstNum = 8
+    var secondNum = 3
+
+
+    var inc = firstNum + 1
+
+    println(++firstNum + 1)
+
+
+
+}
