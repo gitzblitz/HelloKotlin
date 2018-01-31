a55a78ee016733ee809344fd81a4ff8b6ab79132
George Ng'ethe <georgegitz@gmail.com>
2018-01-31 11:52:49 +0200

    Subtraction

---
+++ src/Subtraction.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>){
+
+    /* Subtraction*/
+    var firstNum = 5
+    var secondNum = 15
+
+//    var result:Int?
+//
+//    result = firstNum + secondNum
+
+    println("This result is ${firstNum - secondNum}")
+
+
+}
