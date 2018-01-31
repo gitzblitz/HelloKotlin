e127e178072afdae5110e5e0c93a9fee54ab1043
George Ng'ethe <georgegitz@gmail.com>
2018-01-31 14:17:03 +0200

    Remainder

---
+++ src/Remainder.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>){
+
+    /*
+    Remainder(%)
+    */
+    var firstNum = 8
+    var secondNum = 3
+
+//    var result:Int?
+//
+//    result = firstNum + secondNum
+
+    println("This result is ${firstNum % secondNum}")
+
+
+}
