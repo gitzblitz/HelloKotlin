81c61163c02252e069d66a2e16b57b35733fd88f
George Ng'ethe <georgegitz@gmail.com>
2018-01-31 13:11:09 +0200

    Division

---
+++ src/Division.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>){
+
+    /*
+    Division(/)
+    */
+    var firstNum = 1
+    var secondNum = 5
+
+//    var result:Int?
+//
+//    result = firstNum + secondNum
+
+    println("This result is ${firstNum.toFloat() / secondNum}")
+
+
+}
