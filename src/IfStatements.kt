d59ed450f64f73dbcb392270c57578698231c9d7
George Ng'ethe <gitzblitz@users.noreply.github.com>
2018-02-01 13:39:33 +0200

    If statements

---
+++ src/IfStatements.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+/*
+    if statement
+
+ */
+
+    var firstNum = 5
+    var secondNum = 6
+
+//   if (firstNum > secondNum){
+//       println("yes")
+//   }
+    if (firstNum >= secondNum) println("yes") else println("no")
+//    if (firstNum >= secondNum){
+//       println("yes")
+//   } else {
+//        println("no")
+//    }
+
+}
