dcf50fe1a3645b819222e9db6be85ce1fcbbd338
George Ng'ethe <gitzblitz@users.noreply.github.com>
2018-02-01 14:58:49 +0200

    When statement

---
+++ src/When.kt
@@ -0,0 +1,33 @@
+fun main(args: Array<String>) {
+
+    /*
+    When
+     */
+
+    println("Enter a number between 1 - 10")
+    var input = readLine()!!.toInt()
+
+
+    when(input){
+        1 -> println("wrong number")
+        2 -> println("Getting Close")
+        3 -> println("Closer")
+        4 -> println("Hmm..")
+        5 -> println("Well, you are lost")
+        6 -> println("Bingo")
+        else ->{
+            println("Waaayyyy lost")
+        }
+    }
+//    var age: Int = 19
+//
+//    age = 16
+//    when(age){
+//        17 -> println("17")
+//        18 -> println("18")
+//        19 -> println("Yep 19")
+//        else ->{
+//            println("none of the above")
+//        }
+//    }
+}
