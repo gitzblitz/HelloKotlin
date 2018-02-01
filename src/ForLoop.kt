d2f9378c2391efb674886cf5d52ead773405fa53
George Ng'ethe <gitzblitz@users.noreply.github.com>
2018-02-01 23:24:54 +0200

    For loop

---
+++ src/ForLoop.kt
@@ -0,0 +1,15 @@
+fun main (args: Array<String>){
+
+    //for loop
+
+    for (item in 0..100){
+        if (item % 2 == 0){
+            println("Fizz $item")
+        } else if (item % 3 == 0){
+            println("buzz $item")
+        }
+
+    }
+
+
+}
