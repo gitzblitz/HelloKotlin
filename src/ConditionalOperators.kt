9c904e75455088481fbdf9761af73c8660d50214
George Ng'ethe <gitzblitz@users.noreply.github.com>
2018-02-01 12:39:50 +0200

    Conditional operators

---
+++ src/ConditionalOperators.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+
+/*
+* Conditional operators < > == !=  <= >=
+*/
+
+    var firstNum = 5
+    var secondNum = 6
+
+    println(firstNum > secondNum)
+    println(firstNum < secondNum)
+    println(firstNum == secondNum)
+    println(firstNum != secondNum)
+    println(firstNum <= secondNum)
+    println(firstNum >= secondNum)
+
+
+}
