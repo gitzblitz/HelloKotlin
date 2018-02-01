9f7b15e5db59292cf0dfaafb7371e9d4736f4ca5
George Ng'ethe <gitzblitz@users.noreply.github.com>
2018-02-01 23:45:24 +0200

    Do While loop

---
+++ src/DoWhileLoop.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+
+    /*do while loop - run it at least once */
+
+    var counter = 1
+
+    do {
+
+        if (counter% 3 == 0) println("fizz") else println("buzz")
+//        println("hey")
+        counter++
+    } while (counter < 15)
+
+//
+//    while (counter <= 5) {
+//        println("Counting... $counter")
+//
+//        counter++
+//    }
+//    println("Out of while loop")
+}
