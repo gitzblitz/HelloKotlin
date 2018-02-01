dcf5cb893c0cb3cb9300112a8f67025057b7a525
George Ng'ethe <gitzblitz@users.noreply.github.com>
2018-02-01 23:37:58 +0200

    While loop

---
+++ src/WhileLoop.kt
@@ -0,0 +1,12 @@
+fun main (args: Array<String>){
+
+    /*while loop*/
+
+    var counter = 0
+    while (counter <= 5){
+        println("Counting... $counter")
+
+        counter++
+    }
+    println("Out of while loop")
+}
