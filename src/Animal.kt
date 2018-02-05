294aa1433000022c475b878854a25a4ce04d145f
George Ng'ethe <gitzblitz@users.noreply.github.com>
2018-02-05 17:31:08 +0200

    Overloading constructors

--- src/Animal.kt
+++ src/Animal.kt
@@ -12,21 +12,55 @@ open class Animal() {
         println("Object " + this.colour)
         println("Object " + this.numLegs)
     }
+
+    constructor(name: String, colour: String) : this() {
+        this.name = name
+        this.colour = colour
+
+        println("Third constructor " + this.name)
+    }
+
+    fun showAnimal() {
+        println("Name is " + this.name)
+        println("Colour is " + this.colour)
+        println("NumLegs is " + this.numLegs)
+
+    }
 }
 
 
-class Lion() : Animal() {
+class Lion : Animal {
+    var numEyes: Int? = null
+    var age: Int? = null
+
+    constructor(numEyes: Int, age: Int, name: String) : super() {
+        super.name = name
+        this.numEyes = numEyes
+        this.age = age
+
+        println("Lion constructor " + this.numEyes + " " + this.age)
+    }
 
 }
 
 fun main(args: Array<String>) {
 
+//var animal = Animal("Animal", "brown")
+
+    var animal = Animal()
+    animal.colour= "brown"
+    animal.name = "Elephant"
+    animal.numLegs = 4
+
+    var animal2 = Animal("Another animal", "blue", 6)
+//    animal.showAnimal()
+    animal2.showAnimal()
 //    var animal = Animal("Dog", "brown", 4)
-
-    var lion = Lion()
-
-    lion.numLegs = 4
-
-    println("num of legs " + lion.numLegs)
+//
+//    var lion = Lion()
+//
+//    lion.numLegs = 4
+//
+//    println("num of legs " + lion.numLegs)
 
 }
