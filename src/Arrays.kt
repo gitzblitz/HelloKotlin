fb7a8c3c95d8cb7d27da0b137acd35ad1e6b86a7
George Ng'ethe <gitzblitz@users.noreply.github.com>
2018-02-06 10:35:18 +0200

    Arrays part 2

--- src/Arrays.kt
+++ src/Arrays.kt
@@ -1,14 +1,22 @@
 fun main(args: Array<String>) {
 
-    var myArray = Array<Int>(5){2}
+    var myArray = Array<Int>(6) { 2 }
     myArray[0] = 10
     myArray[1] = 8
     myArray[2] = 113
     myArray[3] = 2
     myArray[4] = 100
+    myArray[5] = 1000
 
-    for (element in myArray){
+    //printing objects
+    for (element in myArray) {
         println("item is "+ element )
+    }
+
+    //printing items
+
+    for (index in 0..myArray.size - 1) {
+        println(myArray[index])
     }
 
 }
